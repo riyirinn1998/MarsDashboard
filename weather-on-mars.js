2c7b1c0c789304ca6353adda5eb49a7161e4729c
Glitch (hello-webpage) <none>
2021-01-30 13:14:42 +0000

    🍫⛪️ Checkpoint ./weather-on-mars.js:1079212/2021 ./index.html:1079212/520

---
+++ weather-on-mars.js
@@ -0,0 +1,46 @@
+const app = document.getElementById('weather-data');
+
+        const container = document.createElement('div');
+        container.setAttribute('class', 'container');
+
+        app.appendChild(container);
+
+        var request = new XMLHttpRequest();
+
+        request.open('GET', 'https://api.maas2.jiinxt.com/', true);
+        request.onload = function() {
+
+            var data = JSON.parse(this.response);
+
+            if (request.status >= 200 && request.status < 400) {
+
+                console.log(data.sol + " Sol");
+                console.log(data.season + " Season");
+                console.log(data.min_temp + " Min Temp");
+                console.log(data.max_temp + " Max Temp");
+                console.log(data.atmo_opacity + " Atmospheric Opacity");
+                console.log(data.sunset + " Sunset");
+                console.log(data.sunrise + " Sunrise");
+                console.log(data.min_gts_temp + " Min GTS Temp");
+                console.log(data.max_gts_temp + " Max GTS Temp");
+
+                var dataArray = ["Sol: " + data.sol, "Season: " + data.season, "Min Temperature (Air): " + data.min_temp + " °C", "Max Temperature (Air): " + data.max_temp + " °C", "Atmospheric Opacity: " + data.atmo_opacity, "Sunset: " + data.sunset, "Sunrise: " + data.sunrise, "Min Ground Temperature: " + data.min_gts_temp + " °C", "Max Ground Temperature: " + data.max_gts_temp + " °C"];
+                
+                var arrayLength = dataArray.length;
+                for (var i = 0; i < arrayLength; i++) {
+                    const div = document.createElement('div');
+                    div.setAttribute('class', 'weather-data-element');
+                    div.textContent = dataArray[i];
+                    
+                    container.appendChild(div);
+                    
+                    //Do something
+                }
+
+            } else {
+                console.log("There was an error.")
+                app.appendChild(errorMessage);
+            }
+        }
+
+        request.send();
